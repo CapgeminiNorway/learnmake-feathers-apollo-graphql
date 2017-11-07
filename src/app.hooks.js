b29f6330160049e9cd3b260c69f66a12cebb0f24
Yilmaz Guleryuz <yilmaz@guleryuz.net>
2017-11-07 13:07:33 +0100

    append createdAt, updatedAt

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,19 +1,28 @@
 // Application hooks that run for every service
 const logger = require('./hooks/logger');
+const commonHooks = require('feathers-hooks-common');
+const winstonLogger = require('winston');
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [ commonHooks.setNow('createdAt') ],
+    update: [ commonHooks.setNow('updatedAt') ],
+    patch: [ commonHooks.setNow('updatedAt') ],
     remove: []
   },
 
   after: {
-    all: [ logger() ],
+    all: [
+      logger(),
+      hook => {
+        if (hook.result && hook.result.meta_data) {
+          commonHooks.discard('meta_data');
+        }
+      }
+    ],
     find: [],
     get: [],
     create: [],
